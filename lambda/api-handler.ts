4e5349fde1a6b7795eb3849108ddf4323c955094
vijjuxtra <vijjubox@gmail.com>
2026-03-17 19:29:04 +0530

    Update api-handler.ts

--- lambda/api-handler.ts
+++ lambda/api-handler.ts
@@ -1,1 +1,14 @@
 
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { DynamoDBClient, PutItemCommand, ScanCommand } from '@aws-sdk/client-dynamodb';
+
+const client = new DynamoDBClient({});
+const TABLE_NAME = process.env.TABLE_NAME!;
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  if (event.httpMethod === 'GET') {
+    const data = await client.send(new ScanCommand({ TableName: TABLE_NAME }));
+    return { statusCode: 200, body: JSON.stringify(data.Items) };
+  }
+  return { statusCode: 200, body: 'OK' };
+};
